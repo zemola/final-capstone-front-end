9cf15fadee8f5790f81b6261b6d92dffc5eaf483
Habib Payenda <habiburahmanpayenda@gmail.com>
2023-01-13 19:54:25 +0430

    Adds reservations to Redux store

--- src/app/store.js
+++ src/app/store.js
@@ -1,12 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import carReducer from '../features/car/carSlice';
 import userReducer from '../features/user/userSlice';
+import reservationSlice from '../features/reservations/reservationSlice';
 
 const store = configureStore({
   reducer: {
     users: userReducer,
     cars: carReducer,
-    reservations: {},
+    reservations: reservationSlice,
   },
 });
 
