acaded16c9bab23a63f1739a89e7648623bdb14a
Habib Payenda <habiburahmanpayenda@gmail.com>
2023-01-16 17:38:14 +0430

    Adds styles to home screen

--- src/components/CarCard.js
+++ src/components/CarCard.js
@@ -8,15 +8,15 @@ export default function CarCard({ price, brand, model }) {
     <div className={styles.container}>
       <img src={carImg} className={styles.carImg} alt={model} />
       <div className={styles.carDetails}>
-        <h3>{model}</h3>
-        <p>.................</p>
-        <p>{`Brand: ${brand}`}</p>
-        <p>{`Price: ${price}`}</p>
+        <h3 className={styles.brnadName}>{model}</h3>
+        <p className={styles.dots}>.......................</p>
+        <p className={styles.detailsInfo}>{`Brand: ${brand}`}</p>
+        <p className={styles.detailsInfo}>{`Price: ${price}`}</p>
       </div>
       <div className={styles.socialLinks}>
-        <i className="fa-brands fa-facebook" />
-        <i className="fa-brands fa-twitter" />
-        <i className="fa-brands fa-google" />
+        <i className="fa-brands fa-facebook link" />
+        <i className="fa-brands fa-twitter link" />
+        <i className="fa-brands fa-google link" />
       </div>
     </div>
   );
