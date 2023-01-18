8eb42d28dc9963034137868f97429765980fdbc1
Habib Payenda <habiburahmanpayenda@gmail.com>
2023-01-18 09:53:02 +0430

    Adds pagination in front-end

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,11 +1,19 @@
-import React, { useEffect } from 'react';
-import { useSelector } from 'react-redux';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { fetchCar } from '../features/car/carSlice';
 import CarCard from './CarCard';
 import styles from './HomePage.module.css';
 
 const HomePage = () => {
   const { cars } = useSelector((state) => state.cars);
+  // eslint-disable-next-line prefer-const
+  let [page, setPage] = useState(1);
+  const [prevDisabled, setPrevDisabled] = useState(true);
+  // eslint-disable-next-line no-unused-vars
+  const [nextDisabled, setNextDisabled] = useState(false);
+  const dispatch = useDispatch();
 
   const showCars = () => {
     let carsList;
@@ -20,14 +28,42 @@ const HomePage = () => {
         </Link>
       ));
     } else {
-      carsList = <h1 className={styles.noCars}>There are no cars yet</h1>;
+      carsList = <h1 className={styles.noCars}>There are no more cars yet</h1>;
     }
     return carsList;
   };
 
+  const getCars = () => {
+    dispatch(fetchCar(page));
+  };
+
+  const nextPage = () => {
+    setPage(page += 1);
+    setPrevDisabled(false);
+    dispatch(fetchCar(page));
+    if (cars.length <= 0) {
+      setNextDisabled(true);
+    }
+  };
+
+  const prevPage = () => {
+    if (cars.length > 0) {
+      setNextDisabled(false);
+    }
+    if (page > 1) {
+      setPage(page -= 1);
+    }
+    if (page === 1) {
+      setPrevDisabled(true);
+    }
+
+    dispatch(fetchCar(page));
+  };
+
   useEffect(() => {
+    getCars();
     showCars();
-  });
+  }, []);
 
   return (
     <div className={styles.container}>
@@ -41,7 +77,7 @@ const HomePage = () => {
       </div>
 
       <div className={styles.modelsContainer}>
-        <button type="button" onClick={() => console.log('Prev')} className={styles.pageBtnPrev}>
+        <button type="button" disabled={prevDisabled} onClick={() => prevPage()} className={prevDisabled ? styles.pageBtnDisabledPrev : styles.pageBtnPrev}>
           <p>
             {'<'}
           </p>
@@ -49,7 +85,7 @@ const HomePage = () => {
         <div className={styles.carsContainer}>
           { showCars() }
         </div>
-        <button type="button" onClick={() => console.log('Next')} className={styles.pageBtnNext}>
+        <button type="button" disabled={nextDisabled} onClick={() => nextPage()} className={nextDisabled ? styles.pageBtnDisabledNext : styles.pageBtnNext}>
           <p>
             {'>'}
           </p>
