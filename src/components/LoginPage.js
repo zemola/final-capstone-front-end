f89b3f549981cf66b968c4f7f9905d63c9912c49
Habib Payenda <habiburahmanpayenda@gmail.com>
2023-01-19 09:44:56 +0430

    Adds basic styles to loginPage

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -21,9 +21,9 @@ export default function LoginPage() {
     <div className={styles.container}>
       <h1>Wellcome to Our Page</h1>
       <div className={styles.inputesContainer}>
-        <input type="text" value={userName} onChange={(e) => setUserName(e.target.value)} placeholder="Enter your username" />
-        <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Enter your email" />
-        <button type="button" onClick={() => login(email)}>
+        <input className={styles.input} type="text" value={userName} onChange={(e) => setUserName(e.target.value)} placeholder="Enter your username" />
+        <input className={styles.input} type="text" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Enter your email" />
+        <button className={styles.button} type="button" onClick={() => login(email)}>
           Login
         </button>
       </div>
